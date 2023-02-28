b3069e162ed0453bb25a011ea449e59134d6c697
RICHARD STEPHEN - STUDENT <g00382880@atu.ie>
2023-02-28 18:11:38 +0000

    Added color filter buttons

--- components/Filters/filter.js
+++ components/Filters/filter.js
@@ -11,21 +11,37 @@ export default function Filter(){
 
   const moreButtons = () =>{
     return(
-      <View>
-      <Button title="Another Button" onPress={() => {}} />
+      <View style={styles.subcolor}>
+      <Button title="Red" onPress={() => {}} />
+      <Button title="Green" onPress={() => {}} />
+      <Button title="Yellow" onPress={() => {}} />
+      <Button title="Blue" onPress={() => {}} />
+
       </View>
     )
 
   }
 
   return (
-    <View>
+    <View style={styles.container}>
+      <View style={styles.color}>
       <Button title="Color" onPress={handlePress} />
+      </View>
       {showButton && moreButtons()}
     </View>
   );
 }
 
-// const styles = StyleSheet.create({
-
-// })
+ const styles = StyleSheet.create({
+    container:{
+      display:"flex",
+     // backgroundColor:"red",
+    },
+    color:{
+        marginRight:"auto",
+    },   
+    subcolor:{
+      display:"flex",
+      marginRight:180,
+    }
+ })
