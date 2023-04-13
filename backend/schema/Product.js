65896e3ef83c51694a56e6bf8b3807ad85f05865
Canhebeatg0kutho <G00382880@atu.ie>
2023-04-13 16:54:56 +0100

    updated product schema

--- backend/schema/Product.js
+++ backend/schema/Product.js
@@ -1,9 +1,20 @@
 const Mongoose = require("mongoose")
 const ProductSchema = new Mongoose.Schema({
+    productTitle:{
+        type:String,
+    },
+    color:{
+        type:String,
+    },
+    url:{
+        type:String,
+    },
+    id:{
+        type:Number,
+    },
     productName:{
         type:String,
         unique:true,
-        required:true,
     },
     price: {
         type:Number,
