a629fb723b1100f748695ec33a9f864831edff9f
Lokesh <g00383507@atu.ie>
2023-04-30 16:02:30 +0100

    created a auth for signin and signup, check if working with backend

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 
 
 mongoose.connect('mongodb+srv://UniFashion:Fashion@cluster0.dylj4xi.mongodb.net/?retryWrites=true&w=majority')
-const db = mongoose.connection
+const db = mongoose.connection-
 app.use(bodyParser.json())
 
 db.on('error',(error)=> console.error(error))
@@ -15,8 +15,6 @@ db.once('open',()=> console.error('Connected to database'))
 
 const productRouter = require("./backend/routes/productRoute")
 app.use('/products',productRouter)
-const userRouter = require("./backend/routes/userRoute")
-app.use('/users',userRouter)
 
 app.use((req,res,next)=>{
     res.status(401).send('NOT_FOUND');
