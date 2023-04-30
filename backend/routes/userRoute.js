10e94620578e98a022c11b3f28f40bcb7ce52ed4
Canhebeatg0kutho <G00382880@atu.ie>
2023-04-30 12:17:35 +0100

    authentication added

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,33 +1,50 @@
 const express = require("express");
 const router = express.Router();
-const Users = require("../schema/user")
+const User = require("../schema/user")
 const bcrypt = require("bcryptjs")
 
 
-router.post('/signup',async(req,res)=>{
-    const user = new Users({
-        username:req.body.username,
-        password:req.body.password
+router.post('/register', async (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  try {
+    bcrypt.hash(user.password,10).then(async (hash)=>{
+      const newUser = await User.create({
+        username: user.username,
+        password: hash
+      });
+      res.status(201).json(newUser);
     })
-    try{
-        const newUser = await user.save()
-        res.json(newUser)
-    }catch(err){
-       res.json({message:err.message})
-    }
-})
 
-router.post('/signin', async(req,res)=>{
-    try{
-        const existing = await Users.findOne({username:req.body.username})
-        if(existing){
-          // if(req.body.password == existing.password){
-            res.json(existing)
-        //   }
-        }
-    }catch(err){
-        res.json({message:err.message})
-    }
-})
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
+router.post('/login', async (req,res)=>{
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+    try{
+      const existingUser = await User.findOne({username:user.username})
+      if(!existingUser){
+        res.status(401).json ({
+          message: "Login not successful",
+          error: "User not found",
+        })
+      }else{
+          bcrypt.compare(user.password,existingUser.password).then((result)=>{
+          result ?  res.status(201).json(existingUser) :  res.status(401).json ({message: "Login not successful", error: "Incorrect Password",})
+        })
+      }
+    } catch (err){
+      res.status(400).json({message: err.message})
+    }
+  })
+
+  
 module.exports = router;
